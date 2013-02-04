8c337269c116d65432e19543a43ab8dd41f8948e
webdorado <webdorado@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-04 13:45:19 +0000

    change link
    git-svn-id: https://plugins.svn.wordpress.org/spider-event-calendar/trunk@663198 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Theme_functions.php
+++ Theme_functions.php
@@ -6,17 +6,17 @@ function show_theme_calendar(){
 
     
  
-<div class="updated" style="font-size: 14px; color:red !important"><p><strong><a href="http://webdorado.org/files/fromSpiderCalendarWP.php" target="_blank" style="color:red; text-decoration:none;"> This feature is disabled for the non-commercial version.</a></strong></p></div>
+<div class="updated" style="font-size: 14px; color:red !important"><p><strong><a href="http://web-dorado.com/files/fromSpiderCalendarWP.php" target="_blank" style="color:red; text-decoration:none;"> This feature is disabled for the non-commercial version.</a></strong></p></div>
    <table width="95%">
                 <tr>
-        <td width="100%" style="font-size:14px; font-weight:bold"><a href="http://webdorado.org/step-4-adding-themes/spider-calendar-wordpress-guide-step-4-1.html" target="_blank" style="color:blue; text-decoration:none;">User Manual</a><br>
+        <td width="100%" style="font-size:14px; font-weight:bold"><a href="http://web-dorado.com/step-4-adding-themes/spider-calendar-wordpress-guide-step-4-1.html" target="_blank" style="color:blue; text-decoration:none;">User Manual</a><br>
 This section allows you to create/edit themes for the calendars.<br /> This feature is disabled for
-the non-commercial version. <a href="http://webdorado.org/step-4-adding-themes/spider-calendar-wordpress-guide-step-4-1.html" target="_blank" style="color:blue; text-decoration:none;">More...</a>
+the non-commercial version. <a href="http://web-dorado.com/step-4-adding-themes/spider-calendar-wordpress-guide-step-4-1.html" target="_blank" style="color:blue; text-decoration:none;">More...</a>
 <br />
-Here are some examples of 11 standard templates included in the commercial version.<a href="http://wpdemo.webdorado.org/spider-calendar/" target="_blank" style="color:blue; text-decoration:none;">Demo</a></td>
+Here are some examples of 11 standard templates included in the commercial version.<a href="http://wpdemo.web-dorado.com/spider-calendar/" target="_blank" style="color:blue; text-decoration:none;">Demo</a></td>
             <td colspan="7" align="right" style="font-size:16px;">
-              <a href="http://webdorado.org/files/fromSpiderCalendarWP.php" target="_blank" style="color:red; text-decoration:none;">
-            <img src="<?php echo plugins_url('images/header.png',__FILE__) ?>" border="0" alt="http://webdorado.org/files/fromSpiderCalendarWP.php" width="215"><br>
+              <a href="http://web-dorado.com/files/fromSpiderCalendarWP.php" target="_blank" style="color:red; text-decoration:none;">
+            <img src="<?php echo plugins_url('images/header.png',__FILE__) ?>" border="0" alt="http://web-dorado.com/files/fromSpiderCalendarWP.php" width="215"><br>
             Get the full version&nbsp;&nbsp;&nbsp;&nbsp;
             </a>
  			 </td>
